405fc9b65d3f0b08e840ef083245ce60eef75be3
patrick <patewalsh@yahoo.com>
2023-11-07 18:55:12 -0500

    task 2 complete

--- frontend/index.js
+++ frontend/index.js
@@ -3,6 +3,7 @@ function moduleProject2() {
   // 👇 WORK WORK BELOW THIS LINE 👇
   let startTime = new Date().getTime() // Record start time
 
+
   function getTimeElapsed() { // To be used at end of game to get elapsed time
     let currentTime = new Date().getTime()
     return currentTime - startTime
@@ -37,6 +38,11 @@ function moduleProject2() {
       row.appendChild(square)
       square.addEventListener('click', () => {
         // 👉 TASK 2 - Use a click handler to target a square 👈
+        if (!square.classList.contains('targeted')) {
+        document.querySelector ('.targeted').classList.remove('targeted')
+        square.classList.add ('targeted')
+      
+    }
       })
     }
   }
@@ -69,6 +75,8 @@ function moduleProject2() {
     // 👉 TASK 4 - Use the space bar to exterminate a mosquito 👈
 
     // 👉 TASK 5 - End the game 👈
+   
+
   })
   // 👆 WORK WORK ABOVE THIS LINE 👆
 }
